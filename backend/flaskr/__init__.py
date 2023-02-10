65018ff71483b75151ac5ea4f43d8232435f96df
Pedro Marcelino <pmarcelino@gmail.com>
2023-02-10 06:04:32 +0000

    rebuild test environment; do first endpoint

--- backend/flaskr/__init__.py
+++ backend/flaskr/__init__.py
@@ -11,22 +11,43 @@ QUESTIONS_PER_PAGE = 10
 def create_app(test_config=None):
     # create and configure the app
     app = Flask(__name__)
+    
+
+    if test_config:
+        app.config.from_mapping(test_config)
+    else:
+        app.config.from_object('config')
+    
     setup_db(app)
 
     """
     @TODO: Set up CORS. Allow '*' for origins. Delete the sample route after completing the TODOs
     """
+    CORS(app)
 
     """
     @TODO: Use the after_request decorator to set Access-Control-Allow
     """
+    @app.after_request
+    def after_request(response):
+        response.headers.add(
+            "Access-Control-Allow-Headers", "Content-Type,Authorization,true"
+        )
+        response.headers.add(
+            "Access-Control-Allow-Methods", "GET,PUT,POST,DELETE,OPTIONS"
+        )
+        return response
 
     """
     @TODO:
     Create an endpoint to handle GET requests
     for all available categories.
     """
-
+    @app.route("/categories")
+    def retrieve_categories():
+        categories = list(set([category.type for category in Category.query.all()]))
+        
+        return jsonify({"success":True, "categories":categories, "total_categories":len(categories)})
 
     """
     @TODO:
--- backend/test_flaskr.py
+++ backend/test_flaskr.py
@@ -6,25 +6,20 @@ from flask_sqlalchemy import SQLAlchemy
 from flaskr import create_app
 from models import setup_db, Question, Category
 
-
 class TriviaTestCase(unittest.TestCase):
     """This class represents the trivia test case"""
 
     def setUp(self):
         """Define test variables and initialize app."""
-        self.app = create_app()
-        self.client = self.app.test_client
         self.database_name = "trivia_test"
-        self.database_path = "postgres://{}/{}".format('localhost:5432', self.database_name)
-        setup_db(self.app, self.database_path)
-
-        # binds the app to the current context
-        with self.app.app_context():
-            self.db = SQLAlchemy()
-            self.db.init_app(self.app)
-            # create all tables
-            self.db.create_all()
-    
+        self.database_path = "postgresql://{}/{}".format('localhost:5432', self.database_name)
+        test_config = {
+            'SQLALCHEMY_DATABASE_URI': self.database_path,
+            'SQLALCHEMY_TRACK_MODIFICATIONS': False
+            }
+        self.app = create_app(test_config)
+        self.client = self.app.test_client
+        
     def tearDown(self):
         """Executed after reach test"""
         pass
@@ -33,6 +28,8 @@ class TriviaTestCase(unittest.TestCase):
     TODO
     Write at least one test for each test for successful operation and for expected errors.
     """
+    def test_test(self):
+        pass
 
 
 # Make the tests conveniently executable
